29092e560fe720ab68cbbb2c40409c5753ef0063
dizikjh <39389059+dizikjh@users.noreply.github.com>
2021-06-05 15:04:24 +1200

    JS: ArrowUp

--- main.js
+++ main.js
@@ -48,6 +48,26 @@ document.addEventListener('scroll',()=>{
     home.style.opacity = 1-window.scrollY / homeHeight;
 });
 
+/* Show "arrow up" button when scrolling down */
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll',()=>{
+    if(window.scrollY > homeHeight/2){
+        arrowUp.classList.add('visible');
+    }else{;
+        arrowUp.classList.remove('visible');
+    }
+});
+
+/*Handle click on the "arrow up" button*/
+arrowUp.addEventListener('click',(event)=>{
+    //link : i class : data-set="#home"
+    // const target = event.target;
+    // const link = target.dataset.link;
+    // const goHome = document.querySelector(link);
+    // goHome.scrollIntoView({behavior:"smooth"});
+    scrollIntoView("#home");
+});
+
 
 function scrollIntoView(selector){
     const scrollTo = document.querySelector(selector);
